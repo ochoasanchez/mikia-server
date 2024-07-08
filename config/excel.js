0da3eacc2e82d21f2dbeff546c9b083378f1f129
Juan <juanluis182@gmail.com>
2024-07-08 13:56:13 -0400

    revision en pantalla

---
+++ config/excel.js
@@ -0,0 +1,17 @@
+module.exports = {
+    config: {
+      "api::client.client": {
+        columns: [
+          "name",
+          "phone",
+          "company",
+          "position",
+          "isPlayer",
+          "level",
+          "email",
+        ],
+        relation: {},
+        locale: "false",
+      },
+    },
+  };
